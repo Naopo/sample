4fef8e2a7b4f8a98c6c3d4f1794285723bd7f682
Naopo <fuzhuoxing@sydl-tech.com>
2018-04-04 07:15:00 +0000

    Add password resets & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -91,13 +91,11 @@ class UsersController extends Controller
     {
       $view = 'email.confirm';
       $data = compact('user');
-      $from = 'fuzhuoxing@sydl-tech.com';
-      $name = 'naopo';
       $to = $user->email;
       $subject = "感谢注册Sample应用！请确认你的邮箱。";
 
-      Mail::send($view, $data, function($message) use ($from,$name,$to,$subject){
-        $message->from($from, $name)->to($to)->subject($subject);
+      Mail::send($view, $data, function($message) use ($to,$subject){
+        $message->to($to)->subject($subject);
       });
     }
 
